e6380a15eb0068c173fc5fd679e2bff8a93af7ac
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-02-28 12:26:44 -0500

    feat: Index.html and Index.ts and Index.js and tsconfig.json

---
+++ Index.ts
@@ -0,0 +1,11 @@
+let namee: string ='Juan';
+let age: number = 12;
+let esMayor: boolean = true;
+let array: string[] = ['Apple'];
+
+function sayHi (name: string): string {
+    return `Hi, ${name}`
+}
+
+console.log(sayHi(namee));
+
---
+++ public/Index.js
@@ -0,0 +1,8 @@
+let namee = 'Juan';
+let age = 12;
+let esMayor = true;
+let array = ['Apple'];
+function sayHi(name) {
+    return `Hi, ${name}`;
+}
+console.log(sayHi(namee));
