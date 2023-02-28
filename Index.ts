c7db17e3ba14660bb437495ce29dc68c8dedd112
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-02-28 12:47:25 -0500

    feat: Index.ts and Index.js

--- Index.ts
+++ Index.ts
@@ -9,3 +9,54 @@ function sayHi (name: string): string {
 
 console.log(sayHi(namee));
 
+//Interfaces
+
+interface User {
+    name: string,
+    id: number
+}
+
+let user: User = {
+    name: 'Pedro',
+    id: 12
+}
+
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name:string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+const user2: User = new UserAccount("Andres", 1);
+
+// unions
+
+type myBool = true | false;
+const myBoolean: boolean = true;
+
+function getLength(obj: string | string[]){
+    return obj.length;
+}
+
+//Generics
+type StringArray = Array<string>;
+let myArray1: StringArray;
+let myArray2: StringArray = ['Apple'];
+
+type ObjectWithNameArray = Array<{name: string}>
+
+interface User  {
+    name: string,
+    id: number
+}
+
+const data: User = {
+        name: 'spiderman',
+        id: 12
+}
+
+
--- public/Index.js
+++ public/Index.js
@@ -6,3 +6,24 @@ function sayHi(name) {
     return `Hi, ${name}`;
 }
 console.log(sayHi(namee));
+let user = {
+    name: 'Pedro',
+    id: 12
+};
+class UserAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const user2 = new UserAccount("Andres", 1);
+const myBoolean = true;
+function getLength(obj) {
+    return obj.length;
+}
+let myArray1;
+let myArray2 = ['Apple'];
+const data = {
+    name: 'spiderman',
+    id: 12
+};
